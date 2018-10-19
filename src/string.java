3ba26b9545db647e2d0802d2ba701cbafe24fc4e
Faruk Becirovic <faruk_becirovic@outlook.com>
2018-10-19 16:46:55 +0200

    version 1

--- src/string.java
+++ src/string.java
@@ -1,4 +1,28 @@
 
 public class string {
+	
+	public int lenght(String str) {
+		return 0;
+	}
+	
+	public void evenPositionElements(String str) {
+		
+	}
+	
+	public void oddPositionsElements(String str) {
+		
+	}
+	
+	public String uppercaseChars(String str) {
+		return null;
+	}
+	
+	public static String lowerCaseChars(String str) {
+		return null;
+	}
+	
+	public String nonLetterChars(String str) {
+		return null;
+	}
 
 }
